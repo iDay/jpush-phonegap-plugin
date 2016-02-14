16b797607b252e28024e20ad5fc94c7a8ae93625
iDay <6155538@qq.com>
2016-02-14 23:21:41 +0800

    编译错误

--- src/android/JPushPlugin.java
+++ src/android/JPushPlugin.java
@@ -460,7 +460,7 @@ public class JPushPlugin extends CordovaPlugin {
 				context.getResources().getIdentifier("title", "id", context.getPackageName()),
 				context.getResources().getIdentifier("text", "id", context.getPackageName()));
 		builder.developerArg0 = "Custom Builder 1";
-		builder.layoutIconDrawable = context.getResources().getIdentifier("jpush_notification_icon", "drawable", context.getPackageName()));
+		builder.layoutIconDrawable = context.getResources().getIdentifier("jpush_notification_icon", "drawable", context.getPackageName());
 		JPushInterface.setPushNotificationBuilder(2, builder);
 		JSONObject obj = new JSONObject();
 		try {
